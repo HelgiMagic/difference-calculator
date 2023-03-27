aada76d4b4f6a2befab067c19db7301deffbcbad
Helgi <helgimagic@yandex.ru>
2023-03-27 11:35:12 +0300

    STEP 6 test: added new fixtures, tests, changed gendiff logic

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -2,18 +2,65 @@
 import genDiff from '../index.js';
 
 const expected = `{
-   - follow: false
-     host: hexlet.io
-   - proxy: 123.234.53.22
-   - timeout: 50
-   + timeout: 20
-   + verbose: true
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
 }`;
-
+const expectedRecursive = `{
+  common: {
+    + follow: false
+      setting1: Value 1
+    - setting2: 200
+    - setting3: true
+    + setting3: null
+    + setting4: blah blah
+    + setting5: {
+          key5: value5
+      }
+      setting6: {
+          doge: {
+            - wow:
+            + wow: so much
+          }
+          key: value
+        + ops: vops
+      }
+  }
+  group1: {
+    - baz: bas
+    + baz: bars
+      foo: bar
+    - nest: {
+          key: value
+      }
+    + nest: str
+  }
+- group2: {
+      abc: 12345
+      deep: {
+          id: 45
+      }
+  }
++ group3: {
+      deep: {
+          id: {
+              number: 45
+          }
+      }
+      fee: 100500
+  }
+}`;
 test('genDiff JSON', () => {
   expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2.json')).toEqual(expected);
 });
 
 test('genDiff YAML', () => {
   expect(genDiff('__fixtures__/file1.yaml', '__fixtures__/file2.yml')).toEqual(expected);
+});
+
+test('gendiff JSON recursive', () => {
+  expect(genDiff('__fixtures__/file1hard.json', '__fixtures__/file2hard.json')).toEqual(expectedRecursive);
 });
--- src/gendiff.js
+++ src/gendiff.js
@@ -9,20 +9,114 @@ const normalizeFilePath = (filepath) => {
   if (isAbsolute(filepath)) return filepath;
   return path.resolve(cwd(), filepath);
 };
+const hasChildren = (node) => has(node, 'children');
+const isObject = (value) => typeof value === 'object' && value !== null && !Array.isArray(value);
+const getName = (item) => item.name;
+const getValue = (item) => item.value;
+const getChange = (item) => item.isChanged;
+const generateSpaces = (spaceCount) => {
+  const result = []
+  for (let i = 0; i < spaceCount; i += 1) {
+    result.push(' ');
+  }
+  return result.join('');
+};
+const generateComparedTree = (obj1, obj2) => {
+  const iter = (object1, object2, depth) => {
+    const keys = [...new Set([...Object.keys(object1), ...Object.keys(object2)])].sort();
 
-const compare = (object1, object2) => {
-  const keys = [...new Set([...Object.keys(object1), ...Object.keys(object2)])].sort();
-  const result = keys.reduce((acc, key) => {
-    if (has(object1, key) && !has(object2, key)) acc.push(`- ${key}: ${object1[key]}`);
-    else if (!has(object1, key) && has(object2, key)) acc.push(`+ ${key}: ${object2[key]}`);
-    else if (object1[key] === object2[key]) acc.push(`  ${key}: ${object1[key]}`);
-    else if (object1[key] !== object2[key]) {
-      acc.push(`- ${key}: ${object1[key]}`);
-      acc.push(`+ ${key}: ${object2[key]}`);
+    const result = keys.map((key) => {
+      let isChanged;
+      const value = [];
+      if (isObject(object1[key]) && isObject(object2[key])) {
+        return {
+          name: key, isChanged: 'changed', value, depth, children: iter(object1[key], object2[key], depth + 1),
+        };
+      } if (isObject(object1[key]) && has(object2, key)) {
+        return {
+          name: key, isChanged: 'changed special', value: [object2[key]], depth, children: iter(object1[key], object1[key], depth + 1),
+        };
+      } if (isObject(object1[key])) {
+        return {
+          name: key, isChanged: 'deleted', value, depth, children: iter(object1[key], object1[key], depth + 1),
+        };
+      }  if (isObject(object2[key])) {
+        return {
+          name: key, isChanged: 'added', value, depth, children: iter(object2[key], object2[key], depth + 1),
+        };
+      }
+      if (has(object1, key) && !has(object2, key)) {
+        isChanged = 'deleted';
+        value.push(object1[key]);
+      } else if (!has(object1, key) && has(object2, key)) {
+        isChanged = 'added';
+        value.push(object2[key]);
+      } else if (object1[key] === object2[key]) {
+        isChanged = 'not changed';
+        value.push(object1[key]);
+      } else if (object1[key] !== object2[key]) {
+        isChanged = 'changed';
+        value.push(object1[key]);
+        value.push(object2[key]);
+      }
+      return {
+        name: key,
+        isChanged,
+        value,
+        depth,
+      };
+    });
+
+    return result;
+  };
+  return iter(obj1, obj2, 0);
+};
+
+const formatTree = (tree, style = 'stylish') => {
+  let space;
+  const result = tree.reduce((acc, item) => {
+    const name = getName(item);
+    const [value, changed] = getValue(item);
+    const change = getChange(item);
+    const spaceCount = item.depth * 4 - 2;
+    space = generateSpaces(spaceCount);
+    if (hasChildren(item) && change === 'changed') {
+      acc.push(`${space}  ${name}: ${formatTree(item.children)}`);
+      return acc;
+    }
+    if (hasChildren(item) && change === 'added') {
+      acc.push(`${space}+ ${name}: ${formatTree(item.children)}`);
+      return acc;
+    }
+    if (hasChildren(item) && change === 'changed special') {
+      acc.push(`${space}- ${name}: ${formatTree(item.children)}`);
+      acc.push(`${space}+ ${name}: ${value}`);
+      return acc;
+    }
+    if (hasChildren(item) && change === 'deleted') {
+      acc.push(`${space}- ${name}: ${formatTree(item.children)}`);
+      return acc;
+    }
+    switch (change) {
+      case 'added':
+        acc.push(`${space}+ ${name}: ${value}`);
+        return acc;
+      case 'deleted':
+        acc.push(`${space}- ${name}: ${value}`);
+        return acc;
+      case 'not changed':
+        acc.push(`${space}  ${name}: ${value}`);
+        return acc;
+      case 'changed':
+        acc.push(`${space}- ${name}: ${value}`);
+        acc.push(`${space}+ ${name}: ${changed}`);
+        return acc;
+      default:
+        console.log('ашипка');
     }
     return acc;
-  }, ['{']);
-  return `${result.join('\n   ')}\n}`;
+  }, [`{`]);
+  return `${result.join('\n  ')}\n${space}}`;
 };
 
 const genDiff = (filepath1, filepath2) => {
@@ -32,7 +126,9 @@ const genDiff = (filepath1, filepath2) => {
   const file2 = fs.readFileSync(normFilepath2);
   const object1 = convertToObject(normFilepath1, file1);
   const object2 = convertToObject(normFilepath2, file2);
-  const result = compare(object1, object2);
+  const tree = generateComparedTree(object1, object2);
+  console.log(tree);
+  const result = formatTree(tree);
   console.log(result);
   return result;
 };
